da34b2be2db20bd316e7132eda2063950fb9c7b3
nunofafe7 <95494528+nunofafe7@users.noreply.github.com>
2022-06-23 16:52:06 +0100

    Updated 23/06

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ typedef std::array<double, 3> vector3;
 
 int main()
 {
-    Field<double> myFieldTest0(2);
-    Field<vector3> myFieldTest1;//(2);
+    Field<double> myFieldTest0(5);
+    //Field<vector3> myFieldTest1(3);
     // myFieldTest1.mag();
    /* RunTime time;
     Mesh mesh(time);*/
