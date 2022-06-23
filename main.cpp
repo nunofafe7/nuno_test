2b6a4bf4ebff086ca6b518e33bd7ef13ad8ec257
nunofafe7 <95494528+nunofafe7@users.noreply.github.com>
2022-06-23 10:07:46 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+#include <vector>
+#include <array>
+
+typedef std::array<double, 3> vector3;
+
+#include "field.h"
+
+int main()
+{
+    Field<double> myFieldTest0;//(2);
+    Field<vector3> myFieldTest1;//(2);
+    // myFieldTest1.mag();
+   /* RunTime time;
+    Mesh mesh(time);*/
+}
+
